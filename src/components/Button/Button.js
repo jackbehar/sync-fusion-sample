12f9f2cddab58ecfa6e84c7cca81555f0cce3e38
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-04-07 09:42:38 -0600

    update

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,18 +1,68 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 
 function Button(props) {
-  return (
-    <>
-      <button
-        {...props}
-      />
-      </>
-  );
+  return <ButtonComponent {...props} />;
 }
 
 Button.propTypes = {
-  variant: PropTypes.oneOf(['base', 'link', 'neutral', 'brand', 'outline-brand', 'destructive', 'success', 'text-destructive', 'icon']),
+  /**
+
+Positions the icon before/after the text content in the Button.
+The possible values are:
+Left: The icon will be positioned to the left of the text content.
+Right: The icon will be positioned to the right of the text content.
+*/
+  iconPosition: PropTypes.oneOf(["Left", "Right"]),
+  /**
+  
+  Defines class/multiple classes separated by a space for the Button that is used to include an icon.
+  Buttons can also include font icon and sprite image.
+  */
+  iconCss: PropTypes.string,
+  /**
+  
+  Specifies a value that indicates whether the Button is disabled or not.
+  */
+  disabled: PropTypes.bool,
+  /**
+  
+  Allows the appearance of the Button to be enhanced and visually appealing when set to true.
+  */
+  isPrimary: PropTypes.bool,
+  /**
+  
+  Defines class/multiple classes separated by a space in the Button element. The Button types, styles, and
+  size can be defined by using
+  this.
+  */
+  cssClass: PropTypes.string,
+  /**
+  
+  Defines the text content of the Button element.
+  */
+  content: PropTypes.string,
+  /**
+  
+  Makes the Button toggle, when set to true. When you click it, the state changes from normal to active.
+  */
+  isToggle: PropTypes.bool,
+  /**
+  
+  Overrides the global culture and localization value for this component. Default global culture is 'en-US'.
+  */
+  locale: PropTypes.string,
+  /**
+  
+  Defines whether to allow the cross-scripting site or not.
+  */
+  enableHtmlSanitizer: PropTypes.bool,
+  /**
+  
+  Triggers once the component rendering is completed.
+  */
+  created: PropTypes.func,
 };
 
 export default Button;
--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -1,7 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
+if (!document.getElementById("sync-fusion-css")) {
+  let css = document.createElement("link");
+  css.setAttribute("href", "https://cdn.syncfusion.com/ej2/material.css");
+  css.setAttribute("rel", "stylesheet");
+  css.setAttribute("id", "sync-fusion-css");
+  document.head.appendChild(css);
+}
+
+if (!document.getElementById("sync-fusion-icons")) {
+  let icons = document.createElement("link");
+  icons.setAttribute("href", "https://cdn.syncfusion.com/ej2/ej2-icons/styles/material.css");
+  icons.setAttribute("rel", "stylesheet");
+  icons.setAttribute("id", "sync-fusion-icons");
+  document.head.appendChild(icons);
+}
+
 export default function UXPinWrapper({ children }) {
   return <>{children}</>;
-
 }
